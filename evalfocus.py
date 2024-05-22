d14a3cafcc09942de21617e8cd1b6d3612bf9ede
remov-b4-flight <remove_b4_flight@icloud.com>
2024-05-23 05:42:58 +0900

    adjust power calc.

--- evalfocus.py
+++ evalfocus.py
@@ -12,12 +12,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Constants
-LAP_KERNEL = 5
 MIN_RESULT = 5
 MAX_RESULT = 255
 SMALL_LS = 2400
 BIG_LS = 4800
 VISUAL_WAIT = 2000
+POWER_RANGE = 8
 MOUTH_DEDUCT = 0.75
 FACE_DEDUCT = 0.9
 # Error code
@@ -32,15 +32,6 @@ class FACE :
     RMOUTH_X = 10 ; RMOUTH_Y = 11
     LMOUTH_X = 12 ; LMOUTH_Y = 13
     TRUSTY = 14
-
-#FACE_X = 0; FACE_Y = 1
-#FACE_WIDTH = 2 ; FACE_HEIGHT = 3
-#FACE_REYE_X = 4 ; FACE_REYE_Y = 5
-#FACE_LEYE_X = 6 ; FACE_LEYE_Y = 7
-#FACE_NOSE_X = 8 ; FACE_NOSE_Y = 9
-#FACE_RMOUTH_X = 10 ; FACE_RMOUTH_Y = 11
-#FACE_LMOUTH_X = 12 ; FACE_LMOUTH_Y = 13
-#FACE_TRUSTY = 14
 
 # Write vlog image to home
 def write_image(file_path, image, sub_dir="vlog") :
@@ -71,6 +62,7 @@ result = 0
 ap = argparse.ArgumentParser(description = "Evaluate image focus.")
 ap.add_argument("file", help = "Image file to process.")
 ap.add_argument("-v", help = "verbose outputs", action = 'count', default = 0)
+ap.add_argument("-k", help = "laplacian kernel", type = int, default = 5)
 ap.add_argument("-g", "--graph", help = "show histgram", action = 'store_true', default = False)
 ap.add_argument("-m", "--model", help = "model", default = "yunet.onnx")
 ap.add_argument("-bm", "--brisque_model", help = "BRISQUE model file", default = "brisque_model_live.yml")
@@ -83,6 +75,7 @@ ap.add_argument("-vl", "--vlog", help = "save image log", action = 'store_true',
 args = vars(ap.parse_args())
 
 verbose = args["v"]
+lap_kernel = args["k"]
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 fd_model = os.path.join(script_path, args["model"])
@@ -104,7 +97,8 @@ if (os.path.isfile(brisque_model) != True) :
 if (os.path.isfile(brisque_model) != True) :
     skip_brisque = True
 
-if (verbose >= 1) : print("input image =", image_path)
+if (verbose >= 1) : 
+    print("input image =", image_path)
 # Read image
 original_image = cv2.imread(image_path)
 if original_image is None :
@@ -117,7 +111,8 @@ if (args["skip_brisque"] != True) :
     brisque_score = round(brisque_array[0], 2)
     if (verbose >= 1) : print("BRISQUE score =", brisque_score)
     if (brisque_score > 80.0) :
-        if (verbose >= 2) : print("Evaluate terminated by low BRISQUE score.")
+        if (verbose >= 2) : 
+            print("Evaluate terminated by low BRISQUE score.")
         sys.exit(MIN_RESULT)
 
 orig_height, orig_width, _ = original_image.shape
@@ -133,11 +128,8 @@ else :
         target_size = (resize_long, int(resize_long / aspect))
     image = cv2.resize(original_image,target_size, interpolation = cv2.INTER_NEAREST_EXACT)
 
-#if (args["visual"]) :
-#    cv2.imshow("resize",image)
-#    cv2.waitKey(VISUAL_WAIT)
-
-if (verbose >= 2) : print("shape =", image.shape)
+if (verbose >= 2) : 
+    print("shape =", image.shape)
 
 # Detect faces
 fd = cv2.FaceDetectorYN_create(fd_model, "", (0,0))
@@ -146,7 +138,8 @@ fd.setInputSize((width, height))
 _, faces = fd.detect(image)
 
 faces_count = len(faces) if faces is not None else 0
-if (verbose >= 1) : print("faces =", faces_count)
+if (verbose >= 1) : 
+    print("faces =", faces_count)
 
 # If any face not found, process entire image.
 faces = faces if faces is not None else [[
@@ -161,17 +154,19 @@ faces = faces if faces is not None else [[
 
 count = 0
 max_power = 0
+max_index = 0
 
 # Loop with detected faces
 for face in faces :
 
-    if (verbose >= 1) : print("area ", count)
+    if (verbose >= 1) : 
+        print("area", count, end=": ")
     face_rmouth_x = int(face[FACE.RMOUTH_X])
-    face_rmouth_y = int(face[FACE.RMOUTH_Y])
+#   face_rmouth_y = int(face[FACE.RMOUTH_Y])
     face_lmouth_x = int(face[FACE.LMOUTH_X])
-    face_lmouth_y = int(face[FACE.LMOUTH_Y])
+#   face_lmouth_y = int(face[FACE.LMOUTH_Y])
     if (faces_count >= 1 and verbose >= 2) :
-        print("\tmouth =({0},{1})".format(face_rmouth_x, face_lmouth_x))
+        print("mouth=({0},{1})".format(face_rmouth_x, face_lmouth_x), end=", ")
     face_trusty = round(face[FACE.TRUSTY], 2) if faces_count >= 1 else 0.0
     #Crop face
     face_x1 = int(face[FACE.X])
@@ -183,30 +178,34 @@ for face in faces :
     #Grayscale conversion
     gray = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
     #Laplacian conversion
-    laplacian = cv2.Laplacian(gray, cv2.CV_8U, LAP_KERNEL)
+    laplacian = cv2.Laplacian(gray, cv2.CV_8U, lap_kernel)
     if (args["laplacian"] and (faces_count >= 1 or verbose >= 3)) :
         cv2.imshow("crop", laplacian)
         cv2.waitKey(VISUAL_WAIT)
     # Get result
     hist, bins = np.histogram(laplacian, bins = 32, range = (0,255))
-    if (verbose >= 3) : print("\thist =", hist)
     # Compute the power
     power = 0
     power_length = len(hist)
-    power_start = int(power_length - 4)
+    power_start = int(power_length - POWER_RANGE)
+    if (verbose >= 3) : 
+        print("hist=", hist[ -1 * POWER_RANGE : ], end=", ")
     for i in range(power_start, power_length) :
         power += hist[i] * i
 
-    if (verbose >= 1) : print("\tpower =", power)
+    if (verbose >= 1) : 
+        print("power=", power,end=", ")
     
     # If no faces not detected results deducted.
-    if (faces_count == 0) : power *= FACE_DEDUCT
+    if (faces_count == 0) : 
+        power *= FACE_DEDUCT
     # If both mouth edge not detected, results deducted.
     elif (face_rmouth_x <= 0 and face_lmouth_x <= 0) : power *= MOUTH_DEDUCT
 
     power = int(power)
 
-    if (verbose >= 1 and faces_count >= 1) : print("\tscore =", face_trusty)
+    if (verbose >= 1 and faces_count >= 1) : 
+        print("trusty=", face_trusty, end=", ")
     if (power > max_power) : 
         max_power = power
         max_index = count
@@ -216,11 +215,11 @@ for face in faces :
 # End loop of faces
 
 max_face = faces[max_index]
-pixel_count = int(max_face[FACE.WIDTH] * max_face[FACE.HEIGHT] / 1000)
+pixel_count = int(max_face[FACE.WIDTH] * max_face[FACE.HEIGHT] / 10000)
 power_kpixel = math.ceil(max_power / pixel_count)
 if (verbose >= 2) :
-    print("Kpixels =", pixel_count)
-    print("power/Kpixels =", power_kpixel)
+    print("10Kpixels=", pixel_count)
+    print("power/10Kpixels=", power_kpixel)
 result = power_kpixel
 
 # Make image log
@@ -238,7 +237,7 @@ if (args["vlog"]) :
     cv2.rectangle(image, box, (255, 0, 0), vlog_line)
     cv2.putText(image, str(face_trusty), (max_x, (max_y - 8)), 
                 cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,255,0), 3)
-    cv2.putText(image, ("Result:" + str(result)), (32, 64), 
+    cv2.putText(image, ("Result=" + str(result)), (32, 64), 
                 cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 192), 6)
     cv2.circle(image, [max_rmouth_x, max_rmouth_y], 5, (255,0,255), -1, cv2.LINE_AA)
     cv2.circle(image, [max_lmouth_x, max_lmouth_y], 5, (255,0,255), -1, cv2.LINE_AA)
@@ -258,5 +257,5 @@ if (result > MAX_RESULT) :
 elif (0 < result < MIN_RESULT) : 
     result = MIN_RESULT
 
-print("result =", result)
+print("result=", result)
 sys.exit(result)
